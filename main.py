208c81a8d570711baca4dc172e7df54abcf5de4d
cycoe <cycoe@cycoe.cc>
2018-06-28 22:22:47 +0800

    初步实现双向切割拼接算法

--- main.py
+++ main.py
@@ -24,45 +24,91 @@
 #                      Buddha Bless, No Bug !
 
 import os
+import random
 
-from modules.ImageUtils import ImageUtils
+from modules.Array import Array
 
 
-def main():
-    image_obj_ = []
+def shred_horizontal():
+    array_ = []
 
     max_left = 0
     max_left_index = 0
-    image_path_ = os.listdir('res/attachments/2/')
+    image_path_ = os.listdir('res/attachments/1/')
     for index in range(len(image_path_)):
-        image_obj = ImageUtils().open('res/attachments/2/' + image_path_[index])
-        image_obj.image_to_array().binary()
-        if sum(image_obj.get_edge(ImageUtils.LEFT)) > max_left:
-            max_left = sum(image_obj.get_edge(ImageUtils.LEFT))
+        array = Array().load_image('res/attachments/1/' + image_path_[index])
+        if sum(array.get_edge(Array.LEFT)) > max_left:
+            max_left = sum(array.get_edge(Array.LEFT))
             max_left_index = index
 
-        image_obj_.append(image_obj)
+        array_.append(array)
 
-    join_image = image_obj_[max_left_index]
-    del image_obj_[max_left_index]
+    join_image = array_[max_left_index]
+    del array_[max_left_index]
 
     while True:
         max_match = 0
         max_match_index = 0
-        for index in range(len(image_obj_)):
-            current_match = join_image.match(image_obj_[index])
+        for index in range(len(array_)):
+            current_match = join_image.match(array_[index])
             if current_match > max_match:
                 max_match = current_match
                 max_match_index = index
 
-        join_image = join_image + image_obj_[max_match_index]
-        print(image_obj_[max_match_index])
-        del image_obj_[max_match_index]
+        join_image = join_image.hjoin(array_[max_match_index])
+        print(array_[max_match_index])
+        del array_[max_match_index]
 
-        if not image_obj_:
+        if not array_:
             break
 
     join_image.convert_to_image('test.jpg')
 
+def shred_h_and_v():
+    # 使用贪心算法
+    # 生成样本池
+    array_pool = []
+    image_path_ = os.listdir('res/attachments/3/')
+    random.shuffle(image_path_)  # 对样本池进行洗牌，测试算法稳定性
+    for index in range(len(image_path_)):
+        array = Array().load_image('res/attachments/3/' + image_path_[index])
+        array_pool.append(array)
+
+    row = 11
+    column = 19
+    array_matrix = [[object for x in range(column)]for y in range(row)]
+    for y in range(row):
+        for x in range(column):
+            max_match = 0
+            max_match_index = 0
+            for index in range(len(array_pool)):
+                match = 0
+                match += array_pool[index].match(None if x == 0 else array_matrix[y][x - 1], Array.LEFT)
+                match += array_pool[index].match(None if y == 0 else array_matrix[y - 1][x], Array.TOP)
+                match += array_pool[index].match(None, Array.RIGHT) if x == column - 1 else 0
+                match += array_pool[index].match(None, Array.BOTTOM) if y == row - 1 else 0
+
+                if match > max_match:
+                    max_match = match
+                    max_match_index = index
+
+            array_matrix[y][x] = array_pool[max_match_index]
+            del array_pool[max_match_index]
+
+
+    # 根据生成的 array_matrix 矩阵阵列合并图片
+    image_join_ = []
+    for y in range(row):
+        image_row = array_matrix[y][0]
+        for x in range(1, column):
+            image_row = image_row.hjoin(array_matrix[y][x])
+        image_join_.append(image_row)
+
+    image_join = image_join_[0]
+    for y in range(1, row):
+        image_join = image_join.vjoin(image_join_[y])
+    image_join.convert_to_image('test.jpg')
+
+
 if __name__ == '__main__':
-    main()
+    shred_h_and_v()
---
+++ modules/Array.py
@@ -0,0 +1,140 @@
+# coding: utf-8
+
+import os
+import numpy as np
+
+from PIL import Image
+
+
+class Array(object):
+
+    # 常量设置，代表矩阵4个方向的边
+    LEFT = 0
+    TOP = 1
+    RIGHT = 2
+    BOTTOM = 3
+
+    def __init__(self):
+        self.image_path = None
+        self.image_obj = None
+        self.image_array = None
+
+        self.width = 0
+        self.height = 0
+
+    def __str__(self):
+        if self.image_path:
+            return 'Array object of {}'.format(self.image_path)
+        else:
+            return 'Empty Array object'
+
+    def hjoin(self, other):
+        """
+        水平合并
+        :param other: <Array> 参与合并的另一个 Array 对象
+        :return: <Array> 合并后的 Array 对象
+        """
+        return Array().load_array(np.hstack((self.image_array, other.image_array)))
+
+    def vjoin(self, other):
+        """
+        垂直合并
+        :param other: <Array> 参与合并的另一个 Array 对象
+        :return: <Array> 合并后的 Array 对象
+        """
+        return Array().load_array(np.vstack((self.image_array, other.image_array)))
+
+    def load_image(self, image_path):
+        """
+        加载图片
+        :param image_path: <str> 图片的路径
+        :return: <Array> self
+        """
+        self.image_path = image_path
+
+        # 检测图片是否存在
+        if not os.path.exists(image_path):
+            print('No such a image')
+            return self
+
+        # 生成 image 对象
+        self.image_obj = Image.open(image_path).convert('RGB')
+        self.width = self.image_obj.size[0]
+        self.height = self.image_obj.size[1]
+        self.image_to_array()
+        return self
+
+    def image_to_array(self):
+        """
+        将图片转换成矩阵
+        :return: <Array> self
+        """
+        # np.sum(array, axis=2)
+        # 沿第二个坐标轴求和
+        # 最终生成图片的灰度矩阵
+        self.image_array = np.sum(np.asarray(self.image_obj), axis=2) // 3
+        return self
+
+    def binary(self):
+        """
+        图片二值化
+        :return: <Array> self
+        """
+        self.image_array = self.image_array // 128
+        return self
+
+    def convert_to_image(self, image_path):
+        image_array = self.image_array
+        image = Image.fromarray(image_array.astype(np.uint8))
+        image.save(image_path)
+
+    def load_array(self, array):
+        self.image_array = array
+        return self
+
+    def get_edge(self, direction):
+        """
+        得到图像矩阵的边缘向量
+        :param direction: 对应的边缘
+        :return: <array> 边缘向量
+        """
+        if direction == Array.LEFT:
+            return self.image_array[:, 0]
+        elif direction == Array.TOP:
+            return self.image_array[0]
+        elif direction == Array.RIGHT:
+            return self.image_array[:, self.width - 1]
+        elif direction == Array.BOTTOM:
+            return self.image_array[self.height - 1]
+        else:
+            return self.image_array[:, 0]
+
+    def match(self, image=None, direction=2, kernel=(0.3, 0.4, 0.3)):
+        """
+        计算余弦相似度
+        :param image:
+        :return:
+        """
+        begin_edge = self.get_edge(direction)
+        end_edge = image.get_edge((direction + 2) % 4) if image else np.array([1] * len(begin_edge))
+        # begin_edge = self.convolution(begin_edge, np.array(kernel))
+        # end_edge = self.convolution(end_edge, np.array(kernel))
+
+        cos_theta = np.dot(begin_edge, end_edge) / np.linalg.norm(begin_edge) / np.linalg.norm(end_edge)
+        return 0.5 + 0.5 * cos_theta
+
+    def convolution(self, vector, kernel):
+        kernel = kernel / sum(kernel)
+        vector_len = len(vector)
+        kernel_len = len(kernel)
+        center_index = kernel_len // 2
+
+        for x in range(vector_len):
+            new_value = 0
+            begin_index = center_index - x if center_index - x >= 0 else 0
+            end_index = center_index + vector_len - x if center_index + vector_len - x < kernel_len else kernel_len - 1
+            for y in range(begin_index, end_index):
+                new_value += vector[x + y - center_index] * kernel[y]
+            vector[x] = new_value
+
+        return vector
