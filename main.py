1923bea64a2464f6760981731d16f04146d5c90d
cycoe <cycoe@cycoe.cc>
2018-06-28 16:35:34 +0800

    完成单纵向分隔的情况

--- main.py
+++ main.py
@@ -23,15 +23,46 @@
 #        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #                      Buddha Bless, No Bug !
 
+import os
+
 from modules.ImageUtils import ImageUtils
 
 
 def main():
-    image = ImageUtils()
-    image.open('res/attachments/1/000.bmp')
-    image.image_to_array()
-    print(image.image_array)
+    image_obj_ = []
 
+    max_left = 0
+    max_left_index = 0
+    image_path_ = os.listdir('res/attachments/2/')
+    for index in range(len(image_path_)):
+        image_obj = ImageUtils().open('res/attachments/2/' + image_path_[index])
+        image_obj.image_to_array().binary()
+        if sum(image_obj.get_edge(ImageUtils.LEFT)) > max_left:
+            max_left = sum(image_obj.get_edge(ImageUtils.LEFT))
+            max_left_index = index
+
+        image_obj_.append(image_obj)
+
+    join_image = image_obj_[max_left_index]
+    del image_obj_[max_left_index]
+
+    while True:
+        max_match = 0
+        max_match_index = 0
+        for index in range(len(image_obj_)):
+            current_match = join_image.match(image_obj_[index])
+            if current_match > max_match:
+                max_match = current_match
+                max_match_index = index
+
+        join_image = join_image + image_obj_[max_match_index]
+        print(image_obj_[max_match_index])
+        del image_obj_[max_match_index]
+
+        if not image_obj_:
+            break
+
+    join_image.convert_to_image('test.jpg')
 
 if __name__ == '__main__':
     main()
--- modules/ImageUtils.py
+++ modules/ImageUtils.py
@@ -8,6 +8,11 @@ from PIL import Image
 
 class ImageUtils(object):
 
+    LEFT = 0
+    TOP = 1
+    RIGHT = 2
+    BOTTOM = 3
+
     def __init__(self):
         self.image_path = None
         self.image_obj = None
@@ -21,6 +26,9 @@ class ImageUtils(object):
             return 'ImageUtils object of {}'.format(self.image_path)
         else:
             return 'Empty ImageUtils object'
+
+    def __add__(self, other):
+        return ImageUtils().load_array(np.hstack((self.image_array, other.image_array)))
 
     def open(self, image_path):
         self.image_path = image_path
@@ -42,8 +50,43 @@ class ImageUtils(object):
         self.image_array = np.sum(np.asarray(self.image_obj), axis=2) // 3
         return self
 
+    def convert_to_image(self, image_path):
+        image_array = self.image_array * 255
+        image = Image.fromarray(image_array.astype(np.uint8))
+        image.save(image_path)
+
+    def load_array(self, array):
+        self.image_array = array
+        return self
+
     def binary(self):
-        if not self.image_array:
-            self.image_to_array()
         self.image_array = self.image_array // 128
         return self
+
+    def get_edge(self, axis=0):
+        """
+        得到图像矩阵的边缘向量
+        :param axis: 对应的边缘
+        :return: <array> 边缘向量
+        """
+        if axis == ImageUtils.LEFT:
+            return self.image_array[:, 0]
+        elif axis == ImageUtils.TOP:
+            return self.image_array[0]
+        elif axis == ImageUtils.RIGHT:
+            return self.image_array[:, self.width - 1]
+        elif axis == ImageUtils.BOTTOM:
+            return self.image_array[self.height - 1]
+        else:
+            return self.image_array[:, 0]
+
+    def match(self, image):
+        """
+        计算余弦相似度
+        :param image:
+        :return:
+        """
+        right_edge = self.get_edge(ImageUtils.RIGHT)
+        left_edge = image.get_edge(ImageUtils.LEFT)
+        cos_theta = np.dot(right_edge, left_edge) / np.linalg.norm(right_edge) / np.linalg.norm(left_edge)
+        return 0.5 + 0.5 * cos_theta
